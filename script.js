37ff269a03cd70e9399f0808e026d2d4ee0abbfd
Able Tsang <abletsang@gmail.com>
2017-05-16 09:59:48 -0700

    instead of 'click to play', implemented start, pause, and stop buttons

--- script.js
+++ script.js
@@ -6,28 +6,39 @@ var sessionPlus = document.querySelector(".sessionPlus");
 var sessionTime = document.querySelectorAll(".sessionTime");
 var sessionDisplay = document.querySelector(".sessionDisplay");
 var sessionDisplaySeconds = document.querySelector(".sessionDisplaySeconds");
-var clock = document.querySelector(".clock");
+var play = document.querySelector(".fa-play");
+var pause = document.querySelector(".fa-pause");
+var stop = document.querySelector(".fa-stop");
 // var  = document.querySelector(".");
 
 var breakCount = 5;
 var session = 25;
 var fullSeconds = session*60;
-var counting = false;
+// var counting = false;
+var temp;
 
-clock.addEventListener("click", function() {
-	counting = !counting;
-	if (counting) {
-		var temp = setInterval(countdown, 1000);
-	}
+play.addEventListener("click", function() {
+	// counting = true;
+	temp = setInterval(countdown, 1000);
+});
+
+pause.addEventListener("click", function() {
+	// counting = false;
+	clearInterval(temp);
+});
+
+stop.addEventListener("click", function() {
+	// counting = false;
+	clearInterval(temp);
+	sessionDisplaySeconds.textContent = 00;
+	sessionDisplay.textContent = session;
 });
 
 function countdown() {
 	if (fullSeconds >= 0) {
 		fullSeconds--;
-		var seconds = fullSeconds % 60;
-		var minutes = Math.floor(fullSeconds/60);
-		sessionDisplaySeconds.textContent = seconds;
-		sessionDisplay.textContent = minutes;
+		sessionDisplaySeconds.textContent = fullSeconds % 60;
+		sessionDisplay.textContent = Math.floor(fullSeconds/60);
 		console.log(fullSeconds);
 	}
 }

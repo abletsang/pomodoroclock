08ed165be42aaab287274a671b9fe55905927a1a
Able Tsang <abletsang@gmail.com>
2017-05-16 00:20:32 -0700

    put in countdown functionality

--- script.js
+++ script.js
@@ -5,10 +5,32 @@ var sessionMinus = document.querySelector(".sessionMinus");
 var sessionPlus = document.querySelector(".sessionPlus");
 var sessionTime = document.querySelectorAll(".sessionTime");
 var sessionDisplay = document.querySelector(".sessionDisplay");
+var sessionDisplaySeconds = document.querySelector(".sessionDisplaySeconds");
+var clock = document.querySelector(".clock");
 // var  = document.querySelector(".");
 
 var breakCount = 5;
 var session = 25;
+var fullSeconds = session*60;
+var counting = false;
+
+clock.addEventListener("click", function() {
+	counting = !counting;
+	if (counting) {
+		var temp = setInterval(countdown, 1000);
+	}
+});
+
+function countdown() {
+	if (fullSeconds >= 0) {
+		fullSeconds--;
+		var seconds = fullSeconds % 60;
+		var minutes = Math.floor(fullSeconds/60);
+		sessionDisplaySeconds.textContent = seconds;
+		sessionDisplay.textContent = minutes;
+		console.log(fullSeconds);
+	}
+}
 
 breakMinus.addEventListener("click", function() {
 	if (breakCount > 0) {
@@ -27,6 +49,7 @@ sessionMinus.addEventListener("click", function() {
 		session--;
 		sessionTime.textContent = session;
 		sessionDisplay.textContent = session;
+		fullSeconds = session*60;
 	}
 });
 
@@ -34,4 +57,5 @@ sessionPlus.addEventListener("click", function() {
 		session++;
 		sessionTime.textContent = session;
 		sessionDisplay.textContent = session;
+		fullSeconds = session*60;
 });
